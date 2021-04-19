b17fa3757920a1b0a20cd48d92dff2ffaffde4c6
Oswin Jerome <oswinjeromej@gmail.com>
2021-04-19 22:05:16 +0530

    Login page redesign

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,153 @@
+<ul class="vertical-nav-menu">
+    <li class="app-sidebar__heading">Dashboards</li>
+    <li>
+        <a href="/" class="mm-active">
+            <i class="metismenu-icon pe-7s-rocket"></i>
+            Dashboard
+        </a>
+    </li>
+    <li class="app-sidebar__heading">UI Components</li>
+    @if (Auth::user()->role=="admin")
+        <li>
+            <a href="#">
+                <i class="metismenu-icon pe-7s-users"></i>
+                Users
+                <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+            </a>
+            <ul>
+                <li><a href="/users/create">Add Users</a></li>
+                <li><a href="/users">View Users</a></li>
+            </ul>
+        </li>
+     @endif
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-ticket"></i>
+            Brands
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/brands/create">Add Brand</a></li>
+            <li><a href="/brands">View Brands</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-menu"></i>
+            Categories
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/categories/create">Add Category</a></li>
+            <li><a href="/categories">View Categories</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-plugin"></i>
+            Products
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/products/create">Add Products</a></li>
+            <li><a href="/products">View Products</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-users"></i>
+            Customers
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/customers/create">Add Customer</a></li>
+            <li><a href="/customers">View Customers</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-user"></i>
+            Suppliers
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/suppliers/create">Add Supplier</a></li>
+            <li><a href="/suppliers">View Suppliers</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-news-paper"></i>
+            Sales
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/invoices">View Sales</a></li>
+            <li><a href="/pending_customer_payment">View Pending payment</a></li>
+            <li><a href="/prods/returned">View Returned Products</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-cart"></i>
+            Purchase
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/purchases/create">Add Pruchase</a></li>
+            <li><a href="/purchases">View Purchase</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-cash"></i>
+            Expenses
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/expenses/create">Add Expenses</a></li>
+            <li><a href="/expenses">View Expenses</a></li>
+            <li><a href="/expense-categories/create">Add Expense Category</a></li>
+            <li><a href="/expense-categories">View Expense Category</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-diamond"></i>
+            Barcode
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/barcode">Generate</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="#">
+            <i class="metismenu-icon pe-7s-note2"></i>
+            Reports
+            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+        </a>
+        <ul>
+            <li><a href="/reports/stock">Stock Report</a></li>
+            <li><a href="/reports/stockin">Stock IN Report</a></li>
+            <li><a href="/reports/stock_out">Stock Out Report</a></li>
+            <li><a href="/reports/sales">Sales Report</a></li>
+            <li><a href="/reports/expense">Expense Report</a></li>
+            <li><a href="/reports/c_credit">Customer Credit</a></li>
+            <li><a href="/reports/s_debit">Supplier Debit</a></li>
+        </ul>
+    </li>
+   
+    {{-- <li>
+        <a href="tables-regular.html">
+            <i class="metismenu-icon pe-7s-display2"></i>
+            Tables
+        </a>
+    </li> --}}
+    
+</ul>
--- resources/views/pages/login/login.blade.php
+++ resources/views/pages/login/login.blade.php
@@ -1,47 +1,130 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-        body{
-            height: 100vh;
-            background-color: rebeccapurple;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
 
-        .o-card{
-            max-width: 600px !important;
-            min-width: 400px;
-            background-color: #fff;
-            padding: 15px;
-            border-radius: 15px;
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Login - Billocity - IDEAUX</title>
+    <meta name="viewport"
+    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+        .log{
+            font-family: 'Pacifico';
         }
     </style>
-</head>
+<link href="/main.css" rel="stylesheet"></head>
 <body>
-    <form method="POST" action="login" class="o-card">
-        {{ csrf_field() }}
-        <h3 class="text-center">Login</h3>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input required type="text" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="app-container app-theme-white body-tabs-shadow">
+        <div class="app-container">
+            <div class="h-100">
+                <div class="h-100 no-gutters row">
+                    <div class="d-none d-lg-block col-lg-4">
+                        <div class="slider-light">
+                            <div class="h-100 w-100" style="position: relative;">
+                                <p style="position: absolute;bottom: 50%;left: 50%;
+                                color:#fff;
+                                width: 100%;
+                                font-style: italic;
+                                transform: translate(-30%,50%);z-index: 16;font-size:18px" >
+                                    Powered by, <br>
+                                    <a href="https://ideaux.in" target="_blank" style="color:#fff;text-decoration: none"><span style="font-size:32px;font-weight:bold;font-style: normal;">IDEAUX Technologies</span></a>
+                                </p>
+                                <div class="bg-plum-plate" style="position: absolute;top: 0;bottom: 0;
+                                left: 0;right: 0;z-index: 12;opacity:0.8"></div>
+                                <img style="position: absolute;top: 0;bottom: 0;left: 0;right: 0" src="/assets/images/citydark.jpg" class="w-100 h-100" alt="" srcset="">
+
+                            </div>
+                            {{-- <div class="slick-slider">
+                                <div>
+                                    <div class="position-relative h-100 d-flex justify-content-center align-items-center bg-plum-plate" tabindex="-1">
+                                        <div class="slide-img-bg" style="background-image: url('/assets/images/citydark.jpg');"></div>
+                                        <div class="slider-content">
+                                            <h3>Perfect Balance</h3>
+                                            <p>ArchitectUI is like a dream. Some think it's too good to be true! Extensive
+                                                collection of unified React Boostrap Components and Elements.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="position-relative h-100 d-flex justify-content-center align-items-center bg-premium-dark" tabindex="-1">
+                                        <div class="slide-img-bg" style="background-image: url('/assets/images/citydark.jpg');"></div>
+                                        <div class="slider-content">
+                                            <h3>Scalable, Modular, Consistent</h3>
+                                            <p>Easily exclude the components you don't require. Lightweight, consistent
+                                                Bootstrap based styles across all elements and components
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="position-relative h-100 d-flex justify-content-center align-items-center bg-sunny-morning" tabindex="-1">
+                                        <div class="slide-img-bg" style="background-image: url('/assets/images/citydark.jpg');"></div>
+                                        <div class="slider-content">
+                                            <h3>Complex, but lightweight</h3>
+                                            <p>We've included a lot of components that cover almost all use cases for any type of application.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> --}}
+                        </div>
+                    </div>
+                    <div class="h-100 d-flex bg-white justify-content-center align-items-center col-md-12 col-lg-8">
+                        <div class="mx-auto app-login-box col-sm-12 col-md-10 col-lg-9">
+                            <div class="app-logo mb-4">
+
+                                <a style="font-size:42px;font-weight: bold" class="log mb-3">Billocity</a>
+                            </div>
+                            <h4 class="mb-0">
+                                <span class="d-block">Welcome back,</span>
+                                <span>Please sign in to your account.</span>
+                            </h4>
+                            {{-- <h6 class="mt-3">No account? <a href="javascript:void(0);" class="text-primary">Sign up now</a></h6> --}}
+                            <div class="divider row"></div>
+                            <div>
+                                <form class="" method="POST" action="login">
+                                    {{ csrf_field() }}
+                                    <div class="form-row">
+                                        <div class="col-md-6">
+                                            <div class="position-relative form-group">
+                                                <label for="exampleEmail" class="">Email</label>
+                                                <input required name="email" id="exampleEmail" placeholder="Email here..." type="email" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="position-relative form-group">
+                                                <label for="examplePassword" class="">Password</label>
+                                                <input required name="password" id="examplePassword" placeholder="Password here..." type="password" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="position-relative form-check">
+                                        <input name="check" id="exampleCheck" type="checkbox" class="form-check-input">
+                                        <label for="exampleCheck" class="form-check-label">Keep me logged in</label>
+                                    </div>
+                                    <div class="divider row"></div>
+                                    <div class="d-flex align-items-center">
+                                        <div class="ml-auto">
+                                            {{-- <a href="javascript:void(0);" class="btn-lg btn btn-link">Recover Password</a> --}}
+                                            {{-- <button class="btn btn-primary btn-lg">Login to Dashboard</button> --}}
+                                            <input type="submit" class="btn btn-primary btn-lg" value="Login to Dashboard">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Password</label>
-            <input required type="password" name="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-</body>
+    </div>
+<script type="text/javascript" src="/assets/scripts/main.js"></script></body>
+
 </html>
